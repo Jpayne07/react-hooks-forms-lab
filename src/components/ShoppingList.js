7344b53872330a72888200409a92561cfc1c81e8
Jacob <jacobpayne007@gmail.com>
2024-07-12 20:48:38 -0500

    done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,34 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [dynamicCat, setDynamicCat] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleDynamicChange(event) {
+    setDynamicCat(event.target.value);
+  }
 
-    return item.category === selectedCategory;
+  
+  
+
+  const itemsToDisplay = items.filter((item) => {
+    const categoryMatch = selectedCategory === "All" || item.category === selectedCategory;
+    const dynamicMatch = item.name.toLowerCase().includes(dynamicCat.toLowerCase());
+    return categoryMatch && dynamicMatch;
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm  onItemFormSubmit = {onItemFormSubmit}/>
+      <Filter search={dynamicCat}
+              onCategoryChange={handleCategoryChange} 
+              onSearchChange = {handleDynamicChange} 
+               />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
